faac4065c32ab8bb4bbb0ede4e243d3f1ca37c81
Anmora96 <anmora.2396@gmail.com>
2021-02-08 23:50:04 +0000

    Ejercicio de contador

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import propTypes from "prop-types";
 
 //include bootstrap npm library into the bundle
 import "bootstrap";
@@ -9,7 +10,54 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import { Home } from "./component/home.js";
+function SimpleCounter(props) {
+	return (
+		<div className="bigCounter">
+			<div className="calendar">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="40"
+					height="40"
+					viewBox="0 0 24 24">
+					<path
+						d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm1 12v-6h-2v8h7v-2h-5z"
+						fill="white"
+					/>
+				</svg>
+			</div>
+			<div className="four">{props.digitFour % 10}</div>
+			<div className="three">{props.digitThree % 10}</div>
+			<div className="two">{props.digitTwo % 10}</div>
+			<div className="one">{props.digitOne % 10}</div>
+		</div>
+	);
+}
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+SimpleCounter.propTypes = {
+	digitFour: propTypes.number,
+	digitThree: propTypes.number,
+	digitTwo: propTypes.number,
+	digitOne: propTypes.number
+};
+
+let counter = 0;
+setInterval(function() {
+	const four = Math.floor(counter / 1000);
+	const three = Math.floor(counter / 100);
+	const two = Math.floor(counter / 10);
+	const one = Math.floor(counter / 1);
+	console.log(four, three, two, one);
+	//render your react application
+	counter++;
+	//Esto es para que cada segundo renderice
+	//Solo le estaria pasando un valor nuevo cada vez que este renderizando
+	ReactDOM.render(
+		<SimpleCounter
+			digitOne={one}
+			digitTwo={two}
+			digitThree={three}
+			digitFour={four}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
